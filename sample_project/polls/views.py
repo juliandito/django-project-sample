d346475af0475719db68fa00c06727a4c83e123f
juliandito <vjuliandito@gmail.com>
2022-10-05 16:28:58 +0700

    [ADD] templates

--- sample_project/polls/views.py
+++ sample_project/polls/views.py
@@ -1,9 +1,24 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
-def detail(request, course_id):
-    return HttpResponse(course_id)
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("id %s not found!" % question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
